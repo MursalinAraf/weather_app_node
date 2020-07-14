17efd3f3dffa31ab219052c0b86afdfc78af70c7
MursalinAraf <mursalinaraf1@gmail.com>
2020-07-14 22:43:49 +0600

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,5 @@
 console.log('Client side javascript file is loaded!')
 
-const url = 'http://puzzle.mead.io/puzzle'
-// const weatherurl = 'http://localhost:3000/weather?address=dhaka'
-// fetch(url).then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch(weatherurl).then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         }
-//         else{
-//             console.log(data)
-//         }
-
-//     })
-// })
 
 const weatherform = document.querySelector('form');
 
@@ -26,22 +7,55 @@ weatherform.addEventListener('submit',(e)=>{
     e.preventDefault();
     const weatherinput = document.getElementById('searchinp').value;
     const weatherurl = `/weather?address=${weatherinput}`
-    const first = document.getElementById('first')
-    const second = document.getElementById('second')
+    const address = document.getElementById('address')
+    const temperature = document.getElementById('temperature')
+    const cloud = document.getElementById('cloud')
+    const humidity = document.getElementById('humidity')
+    const pressure = document.getElementById('pressure')
+    const windspeed = document.getElementById('windspeed')
+    const ozone = document.getElementById('ozone')
+    const icon = document.getElementById('icon')
+    const dailycloud = document.getElementById('dailycloud')
+    const highesttemp = document.getElementById('highesttemp')
+    const lowesttemp = document.getElementById('lowesttemp')
+    const dailyhumidity = document.getElementById('dailyhumidity')
+    const dailypressure = document.getElementById('dailypressure')
+    const dailywindspeed = document.getElementById('dailyozone')
+    const dailyozone = document.getElementById('dailyozone')
 
-    first.textContent = 'Loading...'
-    second.textContent = 'Loading...'
+    address.textContent = 'Loading...'
+    // second.textContent = 'Loading...'
 
 fetch(weatherurl).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            first.textContent = data.error
-            second.textContent = ''
+            address.textContent = data.error
+            temperature.textContent = ''
         }
         else{
             console.log(data)
-            first.textContent = data.address
-            second.textContent = data.forecast
+            address.textContent = data.address.toUpperCase();
+            temperature.textContent = data.forecast.temperature
+            cloud.textContent = data.forecast.cloud
+            pressure.textContent = data.forecast.pressure
+            windspeed.textContent = data.forecast.windspeed
+            humidity.textContent = data.forecast.humidity
+            ozone.textContent = data.forecast.ozone
+            icon.textContent = data.forecast.icon
+            dailycloud.textContent = data.forecast.dailycloud
+            highesttemp.textContent = data.forecast.highesttemp
+            lowesttemp.textContent = data.forecast.lowesttemp
+            dailyhumidity.textContent = data.forecast.dailyhumidity
+            dailypressure.textContent = data.forecast.dailypressure
+            dailywindspeed.textContent = data.forecast.dailywindspeed
+            dailyozone.textContent = data.forecast.dailyozone
+
+            if (data.forecast.temperature > 27) {
+                document.body.style.backgroundImage = "url('/img/sunny.jpg')"
+            }
+            if (data.forecast.temperature < 27) {
+                document.body.style.backgroundImage = "url('/img/rainy.jpg')"
+            }
         }
 
     })
--- src/resources/forecast.js
+++ src/resources/forecast.js
@@ -19,9 +19,23 @@ const forecast = (lat,long,callback) => {
             const data = {
                 timezone : response.body.timezone,
                 temperature : response.body.currently.temperature,
-                cloud : response.body.currently.summary
+                cloud : response.body.currently.summary,
+                humidity : response.body.currently.humidity,
+                pressure : response.body.currently.pressure,
+                windspeed : response.body.currently.windSpeed,
+                ozone : response.body.currently.ozone,
+                icon : response.body.currently.icon,
+                dailycloud : response.body.daily.data[0].summary,
+                highesttemp : response.body.daily.data[0].temperatureMax,
+                lowesttemp : response.body.daily.data[0].temperatureMin,
+                dailyhumidity : response.body.daily.data[0].humidity,
+                dailypressure : response.body.daily.data[0].pressure,
+                dailywindspeed : response.body.daily.data[0].windSpeed,
+                dailyozone : response.body.daily.data[0].ozone
+
             }
-            callback(undefined,`In ${data.timezone} Current Temperature Is ${data.temperature} degree celsius And Area Is In ${data.cloud}`)
+            // callback(undefined,`In ${data.timezone} Current Temperature Is ${data.temperature} degree celsius And Area Is In ${data.cloud}`)
+            callback(undefined,data)
         }
 
 
